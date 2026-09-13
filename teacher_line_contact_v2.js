277a2b246419b893aa884a41aa18f0cadb05612f
stepkobetsu-hub <stepkobetsu@gmail.com>
2026-09-13 17:39:05 +0900

    Remove obsolete LINE contact v2 compatibility loader

--- teacher_line_contact_v2.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-(() => {
-  const script=document.createElement('script');
-  script.src='teacher_line_contact_v3.js?v=20260913-session30d-timeout-v1';
-  script.async=false;
-  document.head.appendChild(script);
-})();
